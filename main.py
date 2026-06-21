19f2740f376b17b740e0b84d617ec469b80920be
DanteGaspoz <gatitosaurio13@gmail.com>
2026-06-21 16:28:17 -0300

    Ultimos cambios

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from routes.search import search_bp
+from database.models import db
 
 app = Flask(__name__)
 
+# SQLite
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+    "sqlite:///music_explorer.db"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+# Crear tablas
+with app.app_context():
+    db.create_all()
+
+# Registrar rutas
 app.register_blueprint(search_bp)
 
 if __name__ == "__main__":
--- services/filters.py
+++ services/filters.py
@@ -1,103 +1,34 @@
-from services.lastfm import (
-    get_artist_info,
-    get_similar_artists
-)
-
-from services.musicbrainz import (
-    get_artist_country
-)
+from services.artist_service import get_artist_info
+from services.lastfm_service import get_similar_artists
+from concurrent.futures import ThreadPoolExecutor
+from services.artist_service import get_artist_info
+from services.lastfm_service import get_similar_artists
 
 
-def get_artist_data(name):
+def find_underground_artists(artist, genre, country):
 
-    info = get_artist_info(name)
+    similar_artists = get_similar_artists(artist)
 
-    return {
-        "name": info["name"],
-        "listeners": int(
-            info["stats"]["listeners"]
-        ),
-        "tags": [
-            tag["name"].lower()
-            for tag in info["tags"]["tag"]
-        ],
-        "country": get_artist_country(name)
-    }
+    # limita antes de paralelizar (IMPORTANTE)
+    similar_artists = similar_artists[:15]
 
+    with ThreadPoolExecutor(max_workers=10) as executor:
 
-def is_country_match(
-    artist,
-    country
-):
-
-    return (
-        artist["country"].lower()
-        == country.lower()
-    )
-
-
-def is_genre_match(
-    artist,
-    genre
-):
-
-    return (
-        genre.lower()
-        in artist["tags"]
-    )
-
-
-def is_underground(
-    artist,
-    max_listeners=50000
-):
-
-    return (
-        artist["listeners"]
-        < max_listeners
-    )
-
-
-def find_underground_artists(
-    base_artist,
-    genre_tag,
-    country
-):
-
-    results = []
-
-    artists = (
-        get_similar_artists(
-            base_artist
+        results = list(
+            executor.map(get_artist_info, similar_artists)
         )
-    )
 
-    for artist in artists:
+    # filtros después de obtener datos
+    filtered = []
 
-        try:
+    for info in results:
 
-            data = get_artist_data(
-                artist["name"]
-            )
+        if genre and genre not in (info["genres"] or []):
+            continue
 
-            if (
-                is_country_match(
-                    data,
-                    country
-                )
-                and
-                is_genre_match(
-                    data,
-                    genre_tag
-                )
-                and
-                is_underground(data)
-            ):
+        if country and info["country"] != country:
+            continue
 
-                results.append(data)
+        filtered.append(info)
 
-        except Exception as e:
-
-            print(e)
-
-    return results
+    return filtered
